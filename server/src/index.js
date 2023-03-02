ad723da54453f60ef4a79c8d7dfb25b18a1e9dd6
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-03-02 09:28:24 +0100

    database config

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const server = express();
 
+const config = {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE_NAME,
+    }
+
+const mysql = require('mysql2');
+
+const pool = mysql.createPool(config);
+
+
+
 server.get('/', (req, res) => {
     res.send('Hello World!');
     });
