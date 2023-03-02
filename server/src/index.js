d4c7e1fcbaf7ca5fdd8dc024e5a0549e5a8ecefa
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-03-02 09:40:57 +0100

    post end.point up

--- server/src/index.js
+++ server/src/index.js
@@ -12,10 +12,35 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool(config);
 
+const joi = require('joi')
 
+const schema = joi.object({
+    Namn: joi.string().alphanum().min(3).max(50).required(),
+    Befolkning:joi.number().integer().min(0).max(2000000000).required(),
+    Huvudstad: joi.string().alphanum().min(3).max(50).required()
 
+})
 server.get('/', (req, res) => {
     res.send('Hello World!');
     });
+
+server.post('/api/countries', (req, res) => {
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    const {Namn} = value;
+    pool.execute('SELECT * FROM Land WHERE Namn = ?', [Namn], (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+        else {
+            res.status(200).send(results);
+        }
+    })
+})
+
 
 server.listen(5050);
