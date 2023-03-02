9b718797b9d7d40674e3f199f7a1dc1c6fca25dd
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-03-02 10:15:23 +0100

    Added post endpoint to admin

--- server/src/index.js
+++ server/src/index.js
@@ -1,30 +1,77 @@
 const express = require('express');
 const server = express();
+const mysql = require('mysql2');
+const dotenv = require('dotenv').config();
 
 const config = {
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
-    password: process.env.PASSWORD,
+    password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     }
 
-const mysql = require('mysql2');
+
 
 const pool = mysql.createPool(config);
 
 const joi = require('joi')
 
+server.post('/admin', (req, res) => {
+server.use(express.json());
+
 const schema = joi.object({
-    Namn: joi.string().alphanum().min(3).max(50).required(),
+    password: joi.string().alphanum().min(0).max(6).required(),
+    Namn: joi.string().regex(/^[A-Za-z]+$/).min(3).max(50).required(),
     Befolkning:joi.number().integer().min(0).max(2000000000).required(),
     Huvudstad: joi.string().alphanum().min(3).max(50).required()
 
 })
+
+
+const { error, value } = schema.validate(req.body);
+if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+}
+const {password} = value;
+pool.execute('SELECT * FROM Admins WHERE kod = ?', [password], (err, results) => {
+    if (err) {
+        res.status(500).send(err);
+        return;
+    }
+    else {
+        res.status(200).send(results);
+    }
+})
+
+if (results.length == 0) {
+    res.status(400).send('Fel lösenord');
+    return;
+}
+
+const {Namn, Befolkning, Huvudstad} = value;
+pool.execute('INSERT INTO Land (Namn, Befolkning, Huvudstad) VALUES (?, ?, ?)', [Namn, Befolkning, Huvudstad], (err, results) => {
+    if (err) {
+        res.status(500).send(err);
+            return;
+    }
+    else {
+        res.status(200).send(results);
+    }
+
+})
+})
+
+
+
 server.get('/', (req, res) => {
     res.send('Hello World!');
     });
 
-server.post('/api/countries', (req, res) => {
+server.post('/api', (req, res) => {
+    const schema = joi.object({
+        Namn: joi.string().regex(/^[A-Za-z]+$/).min(3).max(50).required()
+    })
     const { error, value } = schema.validate(req.body);
     if (error) {
         res.status(400).send(error.details[0].message);
