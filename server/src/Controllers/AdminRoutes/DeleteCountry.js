ad8293053d6287e82acd3cc06ce3248060ba11df
TereseBo~ <terese.boderus@yh.nackademin.se>
2023-03-02 15:39:02 +0100

    removed unneccesary comments

--- server/src/Controllers/AdminRoutes/DeleteCountry.js
+++ server/src/Controllers/AdminRoutes/DeleteCountry.js
@@ -1,15 +1,4 @@
 const joi = require('joi');
-// const mysql = require('mysql2');
-// const dotenv = require('dotenv').config();
-
-// const config = {
-//     host: process.env.DATABASE_HOST,
-//     user: process.env.DATABASE_USER,
-//     password: process.env.DATABASE_PASSWORD,
-//     database: process.env.DATABASE_NAME,
-//     }
-
-
 
 const { pool }= require('../../modules/db/pool.js');
 
